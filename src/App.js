897c36801f07301b34745dbef88dbad89181ef97
Novik-Artem <artemgithab@gmail.com>
2022-05-23 17:41:37 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import "./assets/styles/main.scss";
 import MainPage from "./components/pages/MainPage";
 import CardListPage from "./components/pages/CardListPage";
 import CardPage from "./components/pages/CardPage";
+import Header from "./components/molecules/Header";
+import Footer from "./components/molecules/Footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header />
+
         <div className="container">
           <Routes>
-						<Route path="/" element={<MainPage />} />
-						<Route path="/cards" element={<CardListPage />} />
-						<Route path="/card/:id" element={<CardPage />} />
+            <Route path="/" element={<MainPage />} />
+            <Route path="/cards" element={<CardListPage />} />
+            <Route path="/card/:id" element={<CardPage />} />
           </Routes>
         </div>
+
+        <Footer />
       </div>
     </Router>
   );
--- src/components/pages/CardPage/component.js
+++ src/components/pages/CardPage/component.js
@@ -1,41 +1,37 @@
 import { Link } from "react-router-dom";
 import style from "./styles.module.scss";
 import icon from "../../../assets/icons/arrow.svg";
-import Header from "../../molecules/Header"
 
 const CardPage = ({ card }) => {
-	return (
-		<div>
-			<Header/>
-			<div className={style.container}>
-				<Link to="/cards" className={style.link}>
-					<img src={icon} alt="icon_error" className={style.icon} />
-					назад в каталог
-				</Link>
-				<div className={style.content}>
-					<div className={style.image}>
-						<img src={card.img} alt="img_error" />
-					</div>
-					<div className={style.info}>
-						<div className={style.status}>{card.status}</div>
-						<div className={style.name}>{card.name}</div>
-						<div className={style.date}>
-							<span>Дата рождения:</span>
-							{card.birthday}
-						</div>
-						<div className={style.nickname}>
-							<span>Ник-нейм:</span>
-							{card.nickname}
-						</div>
-						<div className={style.quote}>
-							<span>Цитата:</span> Lorem ipsum dolor sit amet, consectetur
-							adipiscing elit. Non, aliquam laoreet vivamus sed. Diam fames mi,
-							quam tellus cursus volutpat velit massa.
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+  return (
+    <div className={style.container}>
+      <Link to="/cards" className={style.link}>
+        <img src={icon} alt="icon_error" className={style.icon} />
+        назад в каталог
+      </Link>
+      <div className={style.content}>
+        <div className={style.image}>
+          <img src={card.img} alt="img_error" />
+        </div>
+        <div className={style.info}>
+          <div className={style.status}>{card.status}</div>
+          <div className={style.name}>{card.name}</div>
+          <div className={style.date}>
+            <span>Дата рождения:</span>
+            {card.birthday}
+          </div>
+          <div className={style.nickname}>
+            <span>Ник-нейм:</span>
+            {card.nickname}
+          </div>
+          <div className={style.quote}>
+            <span>Цитата:</span> Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Non, aliquam laoreet vivamus sed. Diam fames mi,
+            quam tellus cursus volutpat velit massa.
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
